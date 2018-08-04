28b1adf067a21717bae022d4e0b1b82288e7de83
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-08-04 15:38:44 -0700

    hompage added

--- client/src/components/systemItem/systemItem.js
+++ client/src/components/systemItem/systemItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 class SystemItem extends React.Component {
 
-    componentDidMount() {
-        console.log(this.props.value);
-    }
+    // componentDidMount() {
+    //     console.log(this.props.value);
+    // }
 
     render() {
         return (
